8ffda33dd0b8c14310d37a189f729fff1190ff6a
Sai <bandaru.sailakshmi@gmail.com>
2016-07-22 15:51:42 +0530

    Initial Commit

---
+++ LocationWatcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationWatcher
+//
+//  Created by EMM on 22/07/16.
+//  Copyright © 2016 EMM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocationWatcher/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LocationWatcher
+//
+//  Created by EMM on 22/07/16.
+//  Copyright © 2016 EMM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
